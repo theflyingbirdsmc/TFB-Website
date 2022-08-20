e4f98fb745960f8a9a4c122e1afaec138a5381b7
Lucas Christensen <lucas@rosenvold.tech>
2022-08-20 17:38:54 +0200

    Added playert counter

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from flask import Flask, render_template
+from mcstatus import JavaServer
+
+server = JavaServer.lookup("mc.theflyingbirds.net")
+
+status = server.status()
+
+playerCount = status.players.online
 
 app = Flask("__main__")
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", playerCount=playerCount)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)

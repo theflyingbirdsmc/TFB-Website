ba836820b89b2c0606d5355690d144d702e52ebf
Lucas Seidelin Rosenvold Christensen <lucas@rosenvold.tech>
2022-10-29 20:05:46 +0200

    Added wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,11 +7,11 @@ status = server.status()
 
 playerCount = status.players.online
 
-app = Flask("__main__")
+app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html", playerCount=playerCount)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
+    app.run(debug=True)
